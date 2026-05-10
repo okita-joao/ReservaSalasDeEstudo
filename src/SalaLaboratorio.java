eb2ea247053bafc662225a89560c6abc265dbf35
joao <joao.markues@unifesp.br>
2026-05-09 23:44:24 -0300

    adicionei novos atributos e métodos para todas as classes de sala

--- src/SalaLaboratorio.java
+++ src/SalaLaboratorio.java
@@ -1,10 +1,24 @@
 class SalaLaboratorio extends Sala {
-	public SalaLaboratorio(String nome) {
-        super(nome);
+    private int quantidadeComputadores;
+
+    public SalaLaboratorio(
+            String nome,
+            int capacidade,
+            int quantidadeComputadores) {
+
+        super(nome, capacidade);
+
+        this.quantidadeComputadores =
+                quantidadeComputadores;
     }
 
     @Override
     void exibirInfo() {
-        System.out.println("Sala laboratório: " + nome);
+        System.out.println(
+                "Sala laboratório: " + nome
+                + " | Capacidade: " + capacidade
+                + " | Computadores: "
+                + quantidadeComputadores
+        );
     }
 }
--- src/SalaTrabalhoGrupo.java
+++ src/SalaTrabalhoGrupo.java
@@ -1,10 +1,23 @@
 class SalaTrabalhoGrupo extends Sala {
-    public SalaTrabalhoGrupo(String nome) {
-        super(nome);
+    private boolean possuiQuadro;
+
+    public SalaTrabalhoGrupo(
+            String nome,
+            int capacidade,
+            boolean possuiQuadro) {
+
+        super(nome, capacidade);
+
+        this.possuiQuadro = possuiQuadro;
     }
 
     @Override
     void exibirInfo() {
-        System.out.println("Sala de trabalho: " + nome);
+        System.out.println(
+                "Sala de trabalho em grupo: " + nome
+                + " | Capacidade: " + capacidade
+                + " | Quadro: "
+                + possuiQuadro
+        );
     }
 }
